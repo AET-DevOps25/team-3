d4c52a47516755b48a327b219259467e5a0dc0ee
wbaroudi <wa.baroudi9@gmail.com>
2025-06-05 21:11:23 +0200

    Add Elastic IP and Security Group Setup to TF and Adjust Ansible Inventory

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "t3-tf-state"
     key            = "infra/dev/terraform.tfstate"
     region         = "us-east-1"
-    use_lock_table   = true
+    dynamodb_table   = "terraform-locks"
     encrypt        = true
   }
   
@@ -25,12 +25,48 @@ resource "aws_instance" "team-3-server" {
   ami           = "ami-084568db4383264d4"
   instance_type = "t2.micro"
   key_name      = "vockey"
+  vpc_security_group_ids = [aws_security_group.team_3_sg.id]
 
   tags = {
     Name = "Team-3 Server"
   }
 }
 
+# Elastic IP
+resource "aws_eip" "team_3_eip" {
+  instance = aws_instance.team-3-server.id
+  tags = {
+    Name = "Team-3 Server EIP"
+  }
+}
+
+# SG for allowing SSH
+resource "aws_security_group" "team_3_sg" {
+  name        = "team-3-sg"
+  description = "Allow SSH"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # allows SSH from any IP - TODO: assess for security?
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
 output "public_ip" {
-  value = aws_instance.team-3-server.public_ip
+  value = aws_eip.team_3_eip.public_ip
 }
