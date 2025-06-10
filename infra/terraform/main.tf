e079f3bc7e20a98923a71148c8b2eab18909d0fc
wbaroudi <wa.baroudi9@gmail.com>
2025-06-10 15:01:27 +0200

    Add Ingress for Traefik Dashboard

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -70,6 +70,14 @@ resource "aws_security_group" "team_3_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Traefik Dashboard"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
